768cc9c63236ee8c9bd5e4f555733a88dc0db7bf
The Natural <philip.smalls86@gmail.com>
2019-05-26 16:58:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(sentence)
-array = sentence.split(" ").collect { |word| word.reverse!}.join  (" ")
+  array = sentence.split(" ").collect { |word| word.reverse!}.join  (" ")
 end
